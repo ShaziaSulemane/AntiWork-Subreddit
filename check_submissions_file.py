ceac8f03ed5b19bd6d067ba1e8c63b04ae42e553
shazia <shazia.sulemane@gmail.com>
2023-03-27 11:20:38 +0100

    analysis of csv files and logs

--- check_submissions_file.py
+++ check_submissions_file.py
@@ -1,8 +1,19 @@
 import pandas as pd
 import glob
+from datetime import datetime
 
 # create an empty list to store unique values
 unique_ids = []
+now = datetime.now()
+date_time = now.strftime("%m/%d/%Y %H:%M:%S")
+dateforlog = now.strftime("%m_%d_%Y")
+
+existing_posts = []
+log_filename = "Log_" + dateforlog + ".txt"
+
+with open(f"Logs/{log_filename}", "a") as log_file:
+    log_file.write(f"{date_time}: Analysis of CSV post files\n")
+    log_file.close()
 
 # get a list of all csv files in the folder
 csv_files = glob.glob('aw_posts/*.csv')
@@ -13,10 +24,13 @@ for file in csv_files:
     df = pd.read_csv(file)
     # extract the 'id' column and add to unique_ids list
     unique_ids.extend(df['id'].unique())
-    print(len(df['id']))
+    with open(f"Logs/{log_filename}", "a") as log_file:
+        log_file.write(f"{date_time}: {file.title()} has {len(df['id'].tolist())} lines\n")
+        log_file.close()
 
 # convert unique_ids list to a set to remove duplicates
 unique_ids = list(set(unique_ids))
 
-# print the unique ids
-print(len(unique_ids))
+with open(f"Logs/{log_filename}", "a") as log_file:
+    log_file.write(f"{date_time}: All files have {len(unique_ids)} unique ids\n")
+    log_file.close()
